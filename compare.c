66e8fd07056e8d9534d33d8d5af24b1f2a4a5ba8
ruoleyi <ryi1@crimson.ua.edu>
2021-02-15 21:41:16 -0600

    change made

--- compare.c
+++ compare.c
@@ -1,5 +1,6 @@
 #include <string>
 float xmax[34];
+float xmin[34];
 
 void compare(int massPoint){
     cout << "massPoint=" << massPoint << endl;
@@ -36,17 +37,32 @@ TFile *_file2 = TFile::Open(filename2);
     string hist[34] = {"hist_GenEle1_Pt","hist_GenEle1_Eta","hist_GenEle1_Phi","hist_GenEle2_Pt","hist_GenEle2_Eta","hist_GenEle2_Phi","hist_GenJet1_Pt","hist_GenJet1_Eta","hist_GenJet1_Phi","hist_GenJet2_Pt","hist_GenJet2_Eta","hist_GenJet2_Phi","hist_GenJet3_Pt","hist_GenJet3_Eta","hist_GenJet3_Phi","hist_GenJet4_Pt","hist_GenJet4_Eta","hist_GenJet4_Phi","hist_GenJet5_Pt","hist_GenJet5_Eta","hist_GenJet5_Phi","hist_GenLQ1_Pt","hist_GenLQ1_Eta","hist_GenLQ1_Phi","hist_GenLQ1_Mass","hist_GenLQ2_Pt","hist_GenLQ2_Eta","hist_GenLQ2_Phi","hist_GenLQ2_Mass","hist_Jet1_Pt","hist_Jet1_Phi","hist_Jet1_Eta","hist_GenMET_Pt","hist_GenMET_Phi"};
 
     string xtitiles[34]={"GenEle1_Pt [GeV]","GenEle1_Eta","GenEle1_Phi","GenEle2_Pt [GeV]","GenEle2_Eta","GenEle2_Phi","GenJet1_Pt [GeV]","GenJet1_Eta","GenJet1_Phi","GenJet2_Pt [GeV]","GenJet2_Eta","GenJet2_Phi","GenJet3_Pt [GeV]","GenJet3_Eta","GenJet3_Phi","GenJet4_Pt [GeV]","GenJet4_Eta","GenJet4_Phi","GenJet5_Pt [GeV]","GenJet5_Eta","GenJet5_Phi","GenLQ1_Pt [GeV]","GenLQ1_Eta","GenLQ1_Phi","GenLQ1_Mass","GenLQ2_Pt [GeV]","GenLQ2_Eta","GenLQ2_Phi","GenLQ2_Mass","Jet1_Pt [GeV]","Jet1_Phi","Jet1_Eta","GenMET_Pt [GeV]","GenMET_Phi"};
-      
-    float xmin[34]={0,-5,-4,0,-5,-4,0,-5,-4,0,-5,-4,0,-5,-4,0,-5,-4,0,-5,-4,0,-6,-4,400,0,-6,-4,400,0,-4,-4,0,-4};
     
+ 
     if(massPoint==500) {
-        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2000; xmax[7]=5; xmax[8]=4; xmax[9]=2000; xmax[10]=5; xmax[11]=4; xmax[12]=1000; xmax[13]=5; xmax[14]=4; xmax[15]=1000; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=2100; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=2100; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
+        xmin[0]=0; xmin[1]=-5; xmin[2]=-4; xmin[3]=0; xmin[4]=-5; xmin[5]=-4; xmin[6]=0; xmin[7]=-5; xmin[8]=-4; xmin[9]=0; xmin[10]=-5; xmin[11]=-4; xmin[12]=0; xmin[13]=-5; xmin[14]=-4; xmin[15]=0; xmin[16]=-5; xmin[17]=-4; xmin[18]=0; xmin[19]=-5; xmin[20]=-4; xmin[21]=0; xmin[22]=-6; xmin[23]=-4; xmin[24]=200; xmin[25]=0; xmin[26]=-6; xmin[27]=-4; xmin[28]=200; xmin[29]=0; xmin[30]=-4; xmin[31]=-4; xmin[32]=0; xmin[33]=-4;
     }
     else if(massPoint==1000) {
-        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2000; xmax[7]=5; xmax[8]=4; xmax[9]=2000; xmax[10]=5; xmax[11]=4; xmax[12]=1000; xmax[13]=5; xmax[14]=4; xmax[15]=1000; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=2100; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=2100; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
+        xmin[0]=0; xmin[1]=-5; xmin[2]=-4; xmin[3]=0; xmin[4]=-5; xmin[5]=-4; xmin[6]=0; xmin[7]=-5; xmin[8]=-4; xmin[9]=0; xmin[10]=-5; xmin[11]=-4; xmin[12]=0; xmin[13]=-5; xmin[14]=-4; xmin[15]=0; xmin[16]=-5; xmin[17]=-4; xmin[18]=0; xmin[19]=-5; xmin[20]=-4; xmin[21]=0; xmin[22]=-6; xmin[23]=-4; xmin[24]=700; xmin[25]=0; xmin[26]=-6; xmin[27]=-4; xmin[28]=700; xmin[29]=0; xmin[30]=-4; xmin[31]=-4; xmin[32]=0; xmin[33]=-4;
     }
     else if(massPoint==1500) {
-        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2500; xmax[7]=5; xmax[8]=4; xmax[9]=2500; xmax[10]=5; xmax[11]=4; xmax[12]=1500; xmax[13]=5; xmax[14]=4; xmax[15]=1200; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=2100; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=2100; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
+        xmin[0]=0; xmin[1]=-5; xmin[2]=-4; xmin[3]=0; xmin[4]=-5; xmin[5]=-4; xmin[6]=0; xmin[7]=-5; xmin[8]=-4; xmin[9]=0; xmin[10]=-5; xmin[11]=-4; xmin[12]=0; xmin[13]=-5; xmin[14]=-4; xmin[15]=0; xmin[16]=-5; xmin[17]=-4; xmin[18]=0; xmin[19]=-5; xmin[20]=-4; xmin[21]=0; xmin[22]=-6; xmin[23]=-4; xmin[24]=1200; xmin[25]=0; xmin[26]=-6; xmin[27]=-4; xmin[28]=1200; xmin[29]=0; xmin[30]=-4; xmin[31]=-4; xmin[32]=0; xmin[33]=-4;
+    }
+    else if(massPoint==2000) {
+        xmin[0]=0; xmin[1]=-5; xmin[2]=-4; xmin[3]=0; xmin[4]=-5; xmin[5]=-4; xmin[6]=0; xmin[7]=-5; xmin[8]=-4; xmin[9]=0; xmin[10]=-5; xmin[11]=-4; xmin[12]=0; xmin[13]=-5; xmin[14]=-4; xmin[15]=0; xmin[16]=-5; xmin[17]=-4; xmin[18]=0; xmin[19]=-5; xmin[20]=-4; xmin[21]=0; xmin[22]=-6; xmin[23]=-4; xmin[24]=1900; xmin[25]=0; xmin[26]=-6; xmin[27]=-4; xmin[28]=1900; xmin[29]=0; xmin[30]=-4; xmin[31]=-4; xmin[32]=0; xmin[33]=-4;
+    }
+    else {
+        cout << "wrong mass given" << endl;
+    }
+    
+    if(massPoint==500) {
+        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2000; xmax[7]=5; xmax[8]=4; xmax[9]=2000; xmax[10]=5; xmax[11]=4; xmax[12]=1000; xmax[13]=5; xmax[14]=4; xmax[15]=1000; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=800; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=800; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
+    }
+    else if(massPoint==1000) {
+        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2000; xmax[7]=5; xmax[8]=4; xmax[9]=2000; xmax[10]=5; xmax[11]=4; xmax[12]=1000; xmax[13]=5; xmax[14]=4; xmax[15]=1000; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=1300; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=1300; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
+    }
+    else if(massPoint==1500) {
+        xmax[0]=2000; xmax[1]=5; xmax[2]=4; xmax[3]=2000; xmax[4]=5; xmax[5]=4; xmax[6]=2500; xmax[7]=5; xmax[8]=4; xmax[9]=2500; xmax[10]=5; xmax[11]=4; xmax[12]=1500; xmax[13]=5; xmax[14]=4; xmax[15]=1200; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=2500; xmax[22]=6; xmax[23]=4; xmax[24]=1800; xmax[25]=2500; xmax[26]=6; xmax[27]=4; xmax[28]=1800; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
     }
     else if(massPoint==2000) {
         xmax[0]=3000; xmax[1]=5; xmax[2]=4; xmax[3]=3000; xmax[4]=5; xmax[5]=4; xmax[6]=3000; xmax[7]=5; xmax[8]=4; xmax[9]=3000; xmax[10]=5; xmax[11]=4; xmax[12]=2000; xmax[13]=5; xmax[14]=4; xmax[15]=1500; xmax[16]=5; xmax[17]=4; xmax[18]=1000; xmax[19]=5; xmax[20]=4; xmax[21]=3000; xmax[22]=6; xmax[23]=4; xmax[24]=2100; xmax[25]=3000; xmax[26]=6; xmax[27]=4; xmax[28]=2100; xmax[29]=3000; xmax[30]=4; xmax[31]=4; xmax[32]=1000; xmax[33]=4;
